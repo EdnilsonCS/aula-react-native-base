7ff85e94d5b02580df5f931d4628b88670c5ec7c
ednilsoncs <ednilsonsantos43@gmail.com>
2021-02-18 21:24:10 -0300

    feat(routes): adicionado rotas da tela

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import Login from '../screens/Login';
-import {createStackNavigator} from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from '@screens/Login';
+import CreateItemTodo from '@screens/CreateItemTodo';
+import ListTodo from '@screens/ListTodo';
+import CreateItemTodoSuccess from '@screens/CreateItemTodoSuccess';
 
 const Stack = createStackNavigator();
 
-const PublicRoutes = () => {
+const PublicRoutes: React.FC = () => {
   return (
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
-      }}>
+      }}
+    >
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="CreateItemTodo" component={CreateItemTodo} />
+      <Stack.Screen name="ListTodo" component={ListTodo} />
+      <Stack.Screen
+        name="CreateItemTodoSuccess"
+        component={CreateItemTodoSuccess}
+      />
     </Stack.Navigator>
   );
 };
