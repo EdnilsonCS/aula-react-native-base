5504aba686c34b401b348bb017fa2bbcbaf7556d
ednilsoncs <ednilsonsantos43@gmail.com>
2021-02-19 09:04:17 -0300

    chore(components): removido tipagem desnecessaria para o componente de button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,14 +4,13 @@ import { TouchableOpacityProps } from 'react-native';
 import { StyledButton, Label } from './styles';
 
 interface ButtonProps extends TouchableOpacityProps {
-  isLight?: boolean;
   children: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, isLight, ...props }) => {
+const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
   return (
-    <StyledButton isLight={isLight} {...props}>
-      <Label isLight={isLight}>{children}</Label>
+    <StyledButton {...props}>
+      <Label>{children}</Label>
     </StyledButton>
   );
 };
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,21 +2,13 @@ import { TouchableOpacityProps, TextProps } from 'react-native';
 import styled from 'styled-components/native';
 import { colors } from '../../styles';
 
-interface StyledButtonProps extends TouchableOpacityProps {
-  isLight?: boolean;
-}
-
-interface LabelProps extends TextProps {
-  isLight?: boolean;
-}
-
 export const StyledButton = styled.TouchableOpacity.attrs(() => ({
   mode: 'contained',
   uppercase: false,
   labelStyle: {
     fontSize: 14,
   },
-}))<StyledButtonProps>`
+}))`
   background-color: ${colors.black};
   justify-content: center;
   align-items: center;
@@ -25,7 +17,7 @@ export const StyledButton = styled.TouchableOpacity.attrs(() => ({
   border-radius: 5px;
 `;
 
-export const Label = styled.Text<LabelProps>`
+export const Label = styled.Text`
   color: ${colors.white};
   letter-spacing: 1.25px;
   font-size: 14px;
